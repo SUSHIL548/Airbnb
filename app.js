4e266ba2c69b2ff04b34dc849a5304c93fd2b0c4
remi-code <sushil@gmail.com>
2026-08-13 21:59:02 +0530

    Add new feature

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.get("/", (req,res) => {
 })
    
         
-        app.all("*", ( req, res, next ) => { next(new ExpressError(404, "Page Not Found!")); });
+        app.all("/{*splat}", ( req, res, next ) => { next(new ExpressError(404, "Page Not Found!")); });
          app.use((err, req, res, next) => 
             { let { statusCode = 500, message = "Something went wrong!" } = err;
           res.status(statusCode).render("error.ejs", {message}); 
